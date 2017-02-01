1c161e500b627a3690127a30d5be94476283f66e
yulia.shavyrova <yulia.shavyrova@gmail.com>
2017-02-02 01:19:02 +0300

    tests passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,38 @@
+"use strict";
+
 class GuessingGame {
-    constructor() {}
+    constructor(num) {
+        this.secretNum = num;
+    }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
+        this.averageNum = max;
     }
 
     guess() {
-
+        var num = this.averageNum;
+        if (num!=this.secretNum) {
+            num = (this.maxNum-this.minNum)/2 + this.minNum;
+            
+            if (!((num ^ 0) === num)) {
+                num++;
+                num = Math.floor(num);
+            }
+        }
+        this.averageNum = num;
+        console.log("guess = ",this.averageNum);
+        return this.averageNum;
+        
     }
 
     lower() {
-
+        this.maxNum = this.averageNum;
     }
 
     greater() {
-
+        this.minNum = this.averageNum;
     }
 }
-
 module.exports = GuessingGame;
--- test/guessing-game.spec.js
+++ test/guessing-game.spec.js
@@ -1,3 +1,4 @@
+"use strict";
 const GuessingGame = require('../src/guessing-game.js')
 
 describe('GuessingGame', () => {
